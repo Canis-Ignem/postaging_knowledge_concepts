3a7a06b5c3b2849b6092a9126b23c9395e2171a1
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-10-25 16:01:13 +0200

    ACE dropping

--- Data/tagging_verbs.py
+++ Data/tagging_verbs.py
@@ -27,7 +27,7 @@ def get_all_verbs(data, LANGUAGE_DICTS):
         indexes = get_verbs(data, lang, indexes, lang_dict)
     return indexes
 
-def dropping_ge_imperives(data, indexes):
+def dropping_ge_imperives(data):
     indexes = []
     data_copy = data[data["language"] == "de"]
     for i in data_copy.index:
@@ -36,6 +36,13 @@ def dropping_ge_imperives(data, indexes):
 
     data.drop(indexes, inplace=True)
     return data
+
+def removing_ACE_translations(data):
+
+    data_copy = data[data["original"] == "ACE achieving competitive excellence"]
+    data.drop(data_copy.index, inplace=True)
+    return data
+
 
 if __name__ == "__main__":
 
@@ -50,6 +57,9 @@ if __name__ == "__main__":
     print(len(indexes))
     clean_skills.drop(indexes, inplace=True)
     print(clean_skills.shape)
-    clean_skills = dropping_ge_imperives(clean_skills, indexes)
+    clean_skills = dropping_ge_imperives(clean_skills)
     print(clean_skills.shape)
+    clean_skills = removing_ACE_translations(clean_skills)
+    print(clean_skills.shape)
+    clean_skills.reset_index(inplace=True)
     clean_skills.to_csv("Data/clean_skills_verbs.tsv", sep='\t', index=False)
